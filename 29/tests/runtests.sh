5ce7ca3975465e099459cd905d24a3de728c99c0
root <root@localhost.localdomain>
2019-04-10 12:00:01 -0500

    Start exersise 29

---
+++ 29/tests/runtests.sh
@@ -0,0 +1,18 @@
+echo "Running unit Tests:"
+
+for i inm tests/*_tests
+do
+	if test -f $i
+	then
+		if $VALGRIND ./$i 2>> tests/tests.log
+		then
+			echo $i PASS
+		else
+			echo "ERROR in test $i: Here's tests/tests.log"
+			echo "---------"
+			tail tests/tests.log
+			exit 1
+		fi
+	fi
+done
+echo ""	
