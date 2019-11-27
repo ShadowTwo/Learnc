b841eb9debed3af8f857e9d667aa6b5536238bf8
Shadow2 <shadowtwo@gmail.com>
2019-11-27 06:58:07 -0600

    Started Merge Sort - not working

--- 33/src/doublelinkedlist_algos.c
+++ 33/src/doublelinkedlist_algos.c
@@ -7,7 +7,7 @@ int List_bubble_sort(List *list, List_compare cmp)
 	int rc = 0;
 	void *temp = NULL;
 
-	check(list, "List can not be NULL");
+	check(list, "List cannot be NULL");
 	check(list->count, "List is Empty.");
 
 	debug("Start of Bubble Sort...");
@@ -16,11 +16,8 @@ int List_bubble_sort(List *list, List_compare cmp)
 	ListNode *next = list->last->prev;
 	
 	for(x=0;x<(List_count(list)-1);x++)
-	//LIST_FOREACH(list, first, next, cur)
 	{
 		while(cur != list->first)
-		//for(y=List_count(list);y > x; y--)
-		//LIST_FOREACH(list, cur, next, cur2)
 		{
 			rc = cmp(cur->value, cur->prev->value);
 			debug("Cur is '%s', Cur2 is '%s' rc is %d", (char *) cur->value, (char *)cur->prev->value, rc);
@@ -49,7 +46,88 @@ int List_bubble_sort(List *list, List_compare cmp)
 	return 1;
 }
 
-List *List_merge_sort(List *list, List_compare cmp)
+int List_merge_sort(List *list, List_compare cmp)
 {
-	return NULL;
+	check(list, "List cannot be NULL");
+	check(list->count, "List is Empty.");
+
+
+	error:
+	return 1;
+}
+
+List *List_merge_splite(List *list, List_compare cmp)
+{
+	List *l = NULL;
+	List *r = NULL;
+	List *sorted = NULL;
+	
+	if(list->count > 1)
+	{
+		l = Create_list();
+		r = Create_list();
+		
+		int x = 0;
+		Node *cur = list->first;
+
+		for(x=0; x < (list.count/2); x++)
+		{
+			List_push(l, cur->value);
+			cur = cur->next;
+		}
+		for(; x < list.count; x++)
+		{
+			List_push(r, cur->value);
+			cur = cur->next;
+		}
+
+		l = List_merge_splite(l, cmp)
+		r = List_merge_splite(r,cmp)
+
+		return List_merge_merge(list, r, l, cmp)
+	}
+
+	return list;
+
+}
+
+List *List_merge_merge(List *list, List *r, List *l, List_compare cmp)
+{
+	List *Combined = List_create();
+
+	int totalnodes = r->count + L->count;
+	int x= 0;
+
+	Node *cur_r = r->first;
+	Node *cur_l = l->first;
+
+	for(x=0; x < totalnodes; x++)
+	{
+		if(cur_l == NULL)
+		{
+			List_push(Combined, cur_r->value);
+			cur_r = cur_r->next;
+		}
+		else if(cur_r == NULL)
+		{
+			List_push(Combined, cur_l->value);
+			cur_l = cur_l->next;
+		}
+		else if(cmp(cur_l->value, cur_r->value) < 0)
+		{
+			List_push(Combined, cur_l->value;
+			cur_l = cur_l->next;
+		}
+		else
+		{
+			List_push(Combined, cur_r->value;
+			cur_r = cur_r->next;
+		}
+	}
+
+	List_destroy(r);
+	List_destroy(l);
+
+	return Combined;
+
 }
